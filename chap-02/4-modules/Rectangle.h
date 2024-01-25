42f3861943335e847ac8b93ceacec9cb4a79c9cc
Nomehh <mathperriot@gmail.com>
2024-01-25 18:19:01 +0100

    chap 2 - partie 4

--- chap-02/4-modules/Rectangle.h
+++ chap-02/4-modules/Rectangle.h
@@ -3,21 +3,26 @@
 class Rectangle
 {
 public:
-    Rectangle(float length, float width)
-        : _length { length }
-        , _width { width }
-    {}
+    Rectangle(float length, float width);
+    //     : _length { length }
+    //     , _width { width }
+    // {}
+    Rectangle(float size);
+    Rectangle();
+    float get_length() const;
+    float get_width() const;
 
-    float get_length() const { return _length; }
-    float get_width() const { return _width; }
+    void scale(float ratio);
+    // {
+    //     _length *= ratio;
+    //     _width *= ratio;
+    // }
 
-    void scale(float ratio)
-    {
-        _length *= ratio;
-        _width *= ratio;
-    }
+    // inline static float _default_size;
+    static void set_default_size(float default_size);
 
 private:
-    float _length;
-    float _width;
+    float               _length;
+    float               _width;
+    inline static float _default_size;
 };
